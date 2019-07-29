cfcd93552137e42ab0401c9443fddeee93cc04a2
Chester Heng <chesterheng@Chesters-MacBook-Pro.local>
2019-07-29 21:31:06 +0800

    Preparing data for the chart

---
+++ src/settings.js
@@ -0,0 +1,125 @@
+export const countryDataSource = {
+  chart: {
+    // caption options
+    caption: "Top Revenue Earning Countries",
+    xAxisName: "Country",
+    yAxisName: "Revenue (In USD)",
+    theme: "carbon"
+  },
+  data: [
+    {
+      label: "United States",
+      value: "920000"
+    },
+    {
+      label: "China",
+      value: "730000"
+    },
+    {
+      label: "Germany",
+      value: "680000"
+    },
+    {
+      label: "United Kingdom",
+      value: "670000"
+    },
+    {
+      label: "Australia",
+      value: "639000"
+    },
+    {
+      label: "Canada",
+      value: "590000"
+    }
+  ]
+};
+
+export const monthlyDataSource = {
+  chart: {
+    // caption options
+    caption: "Monthly Revenue Trend",
+    xAxisName: "Month",
+    yAxisName: "Revenue (In USD)",
+    theme: "carbon"
+  },
+  data: [
+    {
+      label: "Jan",
+      value: "657000"
+    },
+    {
+      label: "Feb",
+      value: "138000"
+    },
+    {
+      label: "Mar",
+      value: "192000"
+    },
+    {
+      label: "Apr",
+      value: "111000"
+    },
+    {
+      label: "May",
+      value: "438000"
+    },
+    {
+      label: "Jun",
+      value: "292000"
+    },
+    {
+      label: "Jul",
+      value: "365000"
+    },
+    {
+      label: "Aug",
+      value: "649000"
+    },
+    {
+      label: "Sep",
+      value: "746000"
+    },
+    {
+      label: "Oct",
+      value: "803000"
+    },
+    {
+      label: "Nov",
+      value: "876000"
+    },
+    {
+      label: "Dec",
+      value: "730000"
+    }
+  ]
+};
+
+export const productDataSource = {
+  chart: {
+    // caption options
+    caption: "Split of Revnue by Products",
+    theme: "carbon"
+  },
+  data: [
+    {
+      label: "Product 1",
+      value: "1460000"
+    },
+    {
+      label: "Product 2",
+      value: "2190000"
+    },
+    {
+      label: "Product 3",
+      value: "1095000"
+    },
+    {
+      label: "Product 4",
+      value: "1095000"
+    },
+    {
+      label: "Product 5",
+      value: "1460000"
+    }
+  ]
+};
