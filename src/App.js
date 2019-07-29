0ef4d4bdc52b31dfc17869d312b22e86ed43d939
Chester Heng <chesterheng@Chesters-MacBook-Pro.local>
2019-07-29 21:26:19 +0800

    Creating HTML page for the dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Fragment>
+      <div class="chart-row">
+        <div id="country-revenue"></div>
+      </div>
+
+      <div class="chart-row">
+        <div id="monthly-revenue" class="inline-chart"></div>
+        <div id="product-revenue" class="inline-chart"></div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
